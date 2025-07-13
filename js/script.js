dfee3b4e0857272552107dc8c79a0d6983ddd08c
Diya Chunara <diyaachunara@gmail.com>
2025-07-13 13:03:49 +0530

    Update layout, add background image and smooth scroll JS

--- js/script.js
+++ js/script.js
@@ -5,21 +5,6 @@ document.querySelector('#menu-btn').onclick = () =>{
 document.querySelector('#nav-close').onclick = () =>{
     navbar.classList.remove('active');
 }
-
-
-//----------- Toggle Search Form Updated----------------//
-const searchBtn = document.getElementById('search-btn');
-const closeSearch = document.getElementById('close-search');
-const searchForm = document.querySelector('.search-form');
-
-searchBtn.addEventListener('click', () => {
-    searchForm.classList.add('active');
-});
-
-closeSearch.addEventListener('click', () => {
-    searchForm.classList.remove('active');
-});
-
 window.onscroll=()=>{
     navbar.classList.remove('active');
     if(window.scrollY>0){
